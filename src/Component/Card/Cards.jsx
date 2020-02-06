dbe887d7aee13c12535f37a7aeded88483be1e9e
youssefabdullah <youssefabdullah235@gmail.com>
2020-02-06 17:12:46 +0200

    new card

--- src/Component/Card/Cards.jsx
+++ src/Component/Card/Cards.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react"
 import './Cards.css'
 import ImgCard from '../../Img/bde6d9beb1aff1d4c50c7fd4d90cb4bd.jpg'
+import ImgCard2 from '../../Img/178d32f1d9ae922b089ab915291c193b.jpg'
+import ImgCard3 from '../../Img/e1c249c1fe34126d3f4e64fe6f6c0be0.jpg'
+import ImgCard4 from '../../Img/9081514486ed54d75781337149c6808f.jpg'
+import ImgCard5 from '../../Img/54783a3a6907575e63224837cf88ad0d.jpg'
+import ImgCard6 from '../../Img/9a4e74bc7ce9652ea81e693ef1acacdf.jpg'
 class Cards extends Component {
     render() {
         return (
@@ -19,7 +24,7 @@ class Cards extends Component {
                 </div>
                 <div className="cards">
                     <div className="image">
-                        <img src={ImgCard} />
+                        <img src={ImgCard2} />
                     </div>
                     <div className="title">
                         Title Name
@@ -31,7 +36,7 @@ class Cards extends Component {
                 </div>
                 <div className="cards">
                     <div className="image">
-                        <img src={ImgCard} />
+                        <img src={ImgCard3} />
                     </div>
                     <div className="title">
                         Title Name
@@ -43,7 +48,7 @@ class Cards extends Component {
                 </div>
                 <div className="cards">
                     <div className="image">
-                        <img src={ImgCard} />
+                        <img src={ImgCard4} />
                     </div>
                     <div className="title">
                         Title Name
@@ -55,7 +60,7 @@ class Cards extends Component {
                 </div>
                 <div className="cards">
                     <div className="image">
-                        <img src={ImgCard} />
+                        <img src={ImgCard5} />
                     </div>
                     <div className="title">
                         Title Name
@@ -67,7 +72,7 @@ class Cards extends Component {
                 </div>
                 <div className="cards">
                     <div className="image">
-                        <img src={ImgCard} />
+                        <img src={ImgCard6} />
                     </div>
                     <div className="title">
                         Title Name
